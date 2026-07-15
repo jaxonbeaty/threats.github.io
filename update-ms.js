f4b7b4ee1bb7928f7932e5639506ab8aec3f6901
jaxonbeaty <jaxonbeaty@gmail.com>
2026-07-15 16:17:41 -0400

    Update update-ms.js

--- update-ms.js
+++ update-ms.js
@@ -83,6 +83,9 @@ async function updateMicrosoftIssues() {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="pragma" content="no-cache">
+    <meta http-equiv="expires" content="0">
     <title>Microsoft Known Issues</title>
     <style>
         :root {
